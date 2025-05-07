75fa5d5a5c53f92f3a207ad47298daa61679260b
Illia Roman <roman.illia@lll.kpi.ua>
2025-05-07 10:35:31 +0300

    code styling improved

--- BE/src/app.ts
+++ BE/src/app.ts
@@ -1,19 +1,15 @@
-import express from 'express';
-import cors from 'cors';
-import recipeRoutes from './routes/recipes';
-import { PORT } from './config';
+import express from 'express'
+import cors from 'cors'
+import recipeRoutes from './routes/recipes'
+import { PORT } from './config'
 
-// Create Express app
-const app = express();
+const app = express()
 
-// Middlewares
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-// Routes
-app.use('/api', recipeRoutes);
+app.use('/api', recipeRoutes)
 
-// Start server
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+  console.log(`Server running at http://localhost:${PORT}`)
+})
