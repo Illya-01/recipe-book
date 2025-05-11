6e4ec1c1935b58db62cddb8438082af48f54aabe
Illia Roman <roman.illia@lll.kpi.ua>
2025-05-11 11:18:34 +0300

    add TypeScript error suppression comment for CSS import

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// @ts-expect-error typescript cannot import css properly
 import './index.css'
 import App from './App.tsx'
 
